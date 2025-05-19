c0cf5283371fb30061a554223b09fa9480b86e0c
enjoy <enjoy23519105@gmail.com>
2025-05-19 13:31:25 +0800

    javawebOrder

---
+++ src/main/java/dao/OrderDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import model.entity.Order;
+
+public class OrderDAO {
+	
+	// 利用 List 來模擬資料庫/資料表 (稱為:InMemoryDatabase)
+	private static List<Order> orders = new CopyOnWriteArrayList<>();
+	
+	// 存入一筆資訊
+	public void save(Order order) {
+		orders.add(order);
+	}
+	
+	// 刪除一筆資訊
+	public void remove(int index) {
+		orders.remove(index);
+	}
+	
+	// 修改一筆資訊
+	public void update(int index, Order newOrder) {
+		// 把index的資料替換掉
+		orders.set(index, newOrder);
+	}
+	
+	// 取得單筆資料
+	public Order gerOrder(int index) {
+		return orders.get(index);
+	}
+	
+	// 取得所有歷史資料
+	public List<Order> findAll() {
+		return orders;
+	}
+	
+}
